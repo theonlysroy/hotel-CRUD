c3a97e00ad017354a7dc8bb55b01fc3473d036ad
theonlysroy <tirtha13july2002sonofmp@gmail.com>
2024-11-10 15:02:36 +0530

    created room type model and controller

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+import connectDb from "./src/db";
+
+const port = 8001;
+connectDb()
+  .then(() => {
+    app.listen({ port }, () => {
+      console.log(`⚙️ Server running from http://127.0.0.1:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Server failed to start !! \n", error.message);
+    process.exit(1);
+  });
